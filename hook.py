336999f48f3c75af0cab799eb18179f7e6013702
Lei Shi <sl9212@gmail.com>
2017-05-21 15:26:12 -0500

    add host port global val

--- hook.py
+++ hook.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
 import json
 
+HOST = '0.0.0.0'
+PORT - 9700
+
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
-   data = json.loads(request.data)
-   print "New commit by: {}".format(data['commits'][0]['author']['name'])
-   return "OK"
+    data = json.loads(request.data)
+    if 'commits' in data:
+        print "New commit by: {}".format(data['commits'][0]['author']['name'])
+    else:
+        print data
+
+    return "OK"
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host=HOST, port=PORT)
